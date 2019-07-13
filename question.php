12af282047ebb2fc4794489db67f9e86e64d523d
Akshay6890 <47181903+Akshay6890@users.noreply.github.com>
2019-07-13 12:29:25 +0530

    ok

--- question.php
+++ question.php
@@ -18,13 +18,25 @@
 			<a href="#" style="text-decoration:none; color:#8a2be2"><i class="fa fa-tag" style="color:grey"></i> <?php echo $qcat;?></a></h4>
 		</div>
 		<br>
-		<div class="well container">
-			<form class="form-vertical col-lg-8" action="db.php?answer=<?php echo $row['qid'];?>">
-				<h2 style="font-family:Bahnschrift Light; color:#8a2be2">Have an answer?</h2>
-				<textarea class="col-lg-8 form-control" style="resize:none; height:300px"></textarea>
-				<a href="db.php?answer=<?php echo $row['qid'];?>" class="btn btn_web form-control" style="margin-top:15px">Post My Answer</a>
-			</form>
-		</div>
+		<?php
+			$sql="SELECT * FROM faq_comments";
+			$res=mysqli_query($con,$sql);
+			$rows=mysqli_num_rows($res);
+			if($rows==0)
+			{
+				echo '<img src="images/ale.png">';
+			}
+			else
+			{
+				echo '<div class="well container">';
+				echo '<form class="form-vertical col-lg-8" action="db.php?answer='.$row['qid'].'">';
+				echo '<h2 style="font-family:Bahnschrift Light; color:#8a2be2">Have an answer?</h2>';
+				echo '<textarea class="col-lg-8 form-control" style="resize:none; height:300px"></textarea>';
+				echo '<a href="db.php?answer='.$row['qid'].'" class="btn btn_web form-control" style="margin-top:15px">Post My Answer</a>';
+				echo '</form>';
+				echo '</div>';
+			}
+		?>
 	</body>
 <?php
 include 'footer.php';
